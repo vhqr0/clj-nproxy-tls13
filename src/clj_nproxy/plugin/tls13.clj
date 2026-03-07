75b2adbf2b76f7af770429d266aaea6352d8d866
vhqr0 <zq_cmd@163.com>
2026-03-08 00:47:21 +0800

    improve code

--- src/clj_nproxy/plugin/tls13.clj
+++ src/clj_nproxy/plugin/tls13.clj
@@ -7,53 +7,68 @@
 
 (set! clojure.core/*warn-on-reflection* true)
 
+(defn handshake
+  "Do handshake, return new context."
+  [is os context]
+  (loop [context context]
+    (let [{:keys [stage send-bytes]} context]
+      (if (seq send-bytes)
+        (do
+          (run! (partial st/write os) send-bytes)
+          (st/flush os)
+          (recur (dissoc context :send-bytes)))
+        (if (= stage :connected)
+          context
+          (let [{:keys [type content]} (st/read-struct tls13-st/st-record is)]
+            (recur (tls13-ctx/recv-record context type content))))))))
+
+(defn wrap-input-stream
+  "Wrap input stream."
+  [is acontext]
+  (let [read-fn (fn []
+                  (let [{:keys [recv-bytes read-close?]} @acontext]
+                    (if (seq recv-bytes)
+                      (let [b (apply b/cat recv-bytes)]
+                        (swap! acontext update :recv-bytes #(vec (drop (count recv-bytes) %)))
+                        (if-not (zero? (b/length b))
+                          b
+                          (recur)))
+                      (when-not read-close?
+                        (let [{:keys [type content]} (st/read-struct tls13-st/st-record is)]
+                          (swap! acontext tls13-ctx/recv-record type content)
+                          (recur))))))]
+    (BufferedInputStream. (st/read-fn->input-stream read-fn #(st/close is)))))
+
+(defn wrap-output-stream
+  "Wrap output stream."
+  [os acontext]
+  (let [write-fn (fn [b]
+                   (when-not (zero? (b/length b))
+                     (when (:key-update? @acontext)
+                       (swap! acontext tls13-ctx/send-key-update))
+                     (swap! acontext tls13-ctx/send-data b)
+                     (let [{:keys [send-bytes]} @acontext]
+                       (when (seq send-bytes)
+                         (run! (partial st/write os) send-bytes)
+                         (st/flush os)))))
+        close-fn (fn []
+                   (swap! acontext tls13-ctx/send-close-notify)
+                   (let [{:keys [send-bytes]} @acontext]
+                     (when (seq send-bytes)
+                       (run! (partial st/write os) send-bytes)
+                       (st/flush os))
+                     (st/close os)))]
+    (BufferedOutputStream. (st/write-fn->output-stream write-fn close-fn))))
+
 (defn wrap
   "Wrap tls13 on stream."
-  ([stream context]
-   (wrap stream context identity))
-  ([{is :input-stream os :output-stream} context handshake-callback]
-   (let [context (loop [context context]
-                   (let [{:keys [stage send-bytes]} context]
-                     (if (seq send-bytes)
-                       (do
-                         (run! (partial st/write os) send-bytes)
-                         (st/flush os)
-                         (recur (dissoc context :send-bytes)))
-                       (if (= stage :connected)
-                         context
-                         (let [{:keys [type content]} (st/read-struct tls13-st/st-record is)]
-                           (recur (tls13-ctx/recv-record context type content)))))))
+  ([is os context]
+   (wrap is os context identity))
+  ([is os context handshake-callback]
+   (let [context (handshake is os context)
          ;; valid server name, certificate list, etc
          context (handshake-callback context)
-         acontext (atom context)
-         read-fn (fn []
-                   (let [{:keys [recv-bytes read-close?]} @acontext]
-                     (if (seq recv-bytes)
-                       (let [b (apply b/cat recv-bytes)]
-                         (swap! acontext update :recv-bytes #(vec (drop (count recv-bytes) %)))
-                         (if-not (zero? (b/length b))
-                           b
-                           (recur)))
-                       (when-not read-close?
-                         (let [{:keys [type content]} (st/read-struct tls13-st/st-record is)]
-                           (swap! acontext tls13-ctx/recv-record type content)
-                           (recur))))))
-         write-fn (fn [b]
-                    (when-not (zero? (b/length b))
-                      (when (:key-update? @acontext)
-                        (swap! acontext tls13-ctx/send-key-update))
-                      (swap! acontext tls13-ctx/send-data b)
-                      (let [{:keys [send-bytes]} @acontext]
-                        (when (seq send-bytes)
-                          (run! (partial st/write os) send-bytes)
-                          (st/flush os)))))
-         close-fn (fn []
-                    (swap! acontext tls13-ctx/send-close-notify)
-                    (let [{:keys [send-bytes]} @acontext]
-                      (when (seq send-bytes)
-                        (run! (partial st/write os) send-bytes)
-                        (st/flush os))
-                      (st/close os)))]
+         acontext (atom context)]
      {:acontext acontext
-      :input-stream (BufferedInputStream. (st/read-fn->input-stream read-fn #(st/close is)))
-      :output-stream (BufferedOutputStream. (st/write-fn->output-stream write-fn close-fn))})))
+      :input-stream (wrap-input-stream is acontext)
+      :output-stream (wrap-output-stream os acontext)})))
